706fd378135c82fe65f68c1efda326e89554c2d8
sidhardh-joe <sidhardh.22@woxro.com>
2024-11-12 21:45:20 +0530

    somthing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import image from './logo.jpeg'
 
 function App() {
   return (
-    <a href="https://dev.dnjp9ghpt4woh.amplifyapp.com/"  rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
-    <div style={{
+    <a href="https://dev.dnjp9ghpt4woh.amplifyapp.com/" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+      <div style={{
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
         width: "100vw",
         marginTop: "0",
-        cursor: "pointer"  
-    }}>
-         <img src="/logo.jpeg" height="150" width="190" />
-    </div>
-</a>
+        cursor: "pointer"
+      }}>
+        <img src={image} height="150" width="190" />
+      </div>
+    </a>
 
-  
+
+
   );
 }
 
